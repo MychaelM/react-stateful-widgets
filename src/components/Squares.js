0b07f1d3c95d7a73030c5cddc7770983456c5c42
Mychael M <mychaelbmenges@gmail.com>
2020-08-19 17:42:06 -0600

    fixed issue where click on not active square while another square was active did not properly update the state

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -41,7 +41,8 @@ export default function Squares() {
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
     console.log(activeSquare);
-    setActiveSquare(id);
+    setActiveSquare(activeSquare === id ? '' : id);
+    console.log(activeSquare);
   };
 
   return (
